167453708f2c7ecf879500df789ee9c52c41c950
Bearior <zazamheemook@gmail.com>
2021-12-22 12:09:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,31 +38,6 @@ const App = () => {
     }, 200);
   };
 
-  // const handleUpload = () => {
-  //   const uploadTask = storage.ref(`images/${image.name}`).put(image);
-  //   uploadTask.on(
-  //     "state_changed",
-  //     snapshot => {
-  //       const progress = Math.round(
-  //         (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-  //       );
-  //       setProgress(progress);
-  //     },
-  //     error => {
-  //       console.log(error);
-  //     },
-  //     () => {
-  //       storage
-  //         .ref("images")
-  //         .child(image.name)
-  //         .getDownloadURL()
-  //         .then(url => {
-  //           setUrl(url);
-  //         });
-  //     }
-  //   );
-  // };
-
   const takePhoto = (e) => {
     let photo = photoRef.current;
     let strip = stripRef.current;
