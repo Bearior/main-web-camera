4c6137ca63bb0c553087dce4200f4342b1a31ead
Bearior <zazamheemook@gmail.com>
2022-01-25 10:23:52 +0700

    Update History.js

--- src/Pages/History.js
+++ src/Pages/History.js
@@ -42,7 +42,7 @@ const History = () => {
 
 
     const Fetchdata = ()=>{
-      db.collection("users").get().then((querySnapshot) => {
+      db.collection(UserID).get().then((querySnapshot) => {
           console.log("incollection")
           querySnapshot.forEach(element => {
               var contacts = element.data();
