1b369b0faf21847db967e055624e42ff6b41e643
Bearior <zazamheemook@gmail.com>
2022-01-27 10:22:49 +0700

    Update History.js

--- src/Pages/History.js
+++ src/Pages/History.js
@@ -57,7 +57,7 @@ const History = () => {
           <img src = {pictureUrl}
            style={{width: 200, height: 200, borderRadius: 400/ 2 }} />
            <h1 class="History-font"><h2>สวัสดี คุณ </h2>{displayName} </h1>
-           <button class="form" onClick={Fetchdata, this.toggle.bind(this)}>Click to load</button>
+           <button class="form" onClick={Fetchdata}>Click to load</button>
           </center>
       {
           info.map((contacts) => (
