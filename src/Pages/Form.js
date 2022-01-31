78e56a7c02279f72c848ef895edf7418e27bf2ce
Bearior <zazamheemook@gmail.com>
2022-01-31 11:18:11 +0700

    Update Form.js

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -69,7 +69,8 @@ const Contact = () => {
       
         db.collection('PicRe').doc(UserID).get()
         .then(snapshot => setUserDetails(snapshot.data()));
-        console.log(userDetails);
+        let Picscore = userDetails.toString();
+        console.log(Picscore);
 
        
               
