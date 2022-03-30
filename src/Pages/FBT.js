34f42a54cccb960904cc6529be59bde027e9e737
Bearior <zazamheemook@gmail.com>
2022-03-30 20:05:32 +0700

    Update FBT.js

--- src/Pages/FBT.js
+++ src/Pages/FBT.js
@@ -1,5 +1,13 @@
 const FBT = () => {
 window.open("https://main-web-camera.vercel.app/FBT", "_self", "");
-window.close();
+const Close = ()=>{
+    window.close();
+}
+ <center>
+     <p>ลงการนัดหมายสำเร็จ</p>
+     <button onClick={Close}>กดเพื่อปิดหน้านี้</button>
+ </center>
+
+
 }
 export default FBT;
